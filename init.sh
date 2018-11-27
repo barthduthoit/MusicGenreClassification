89f9117875c236f72d12bfe54c457e4b4cd1092e
Barthélemy DUTHOIT <barthelemyduthoit@MacBook-de-Barthelemy-DUTHOIT.local>
2018-11-27 17:34:34 +0000

    complete downloading and mp3 files handling

--- init.sh
+++ init.sh
@@ -3,16 +3,18 @@ cd data/
 mkdir mp3/
 cd mp3/
 
-echo('Downloading first archive...')
+echo Downloading first archive...
 wget http://mi.soi.city.ac.uk/datasets/magnatagatune/mp3.zip.001
-echo('Downloading second archive...')
+echo Downloading second archive...
 wget http://mi.soi.city.ac.uk/datasets/magnatagatune/mp3.zip.002
-echo('Downloading third archive...')
+echo Downloading third archive...
 wget http://mi.soi.city.ac.uk/datasets/magnatagatune/mp3.zip.003
-echo('Extracting archives...')
+echo Extracting archives...
 unzip mp3.zip.001
 unzip mp3.zip.002
 unzip mp3.zip.003
 
+echo Moving mp3 files to data/mp3
 find . -name "*.mp3" -exec mv "{}" ./ \;
 rm -rf */ 
+rm *.zip*
